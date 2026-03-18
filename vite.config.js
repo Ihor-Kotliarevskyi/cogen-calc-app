49b79d3bb68ded26416e4c03c9eb228c26330194
Ihor Kotliarevskyi <jagor0707@gmail.com>
2026-03-18 23:26:16 +0200

    fixx process error

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import process from "node:process";
+
+/// <reference types="node" />
 
 export default defineConfig(({ mode }) => ({
   base:
-    mode === "production" && import.meta.env.GITHUB_ACTIONS === "true"
+    mode === "production" && process.env.GITHUB_ACTIONS
       ? "/cogen-calc-app/"
       : "/",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
