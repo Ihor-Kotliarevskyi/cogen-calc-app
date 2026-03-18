ce671fa154e5f36aedc090261ec303d681935503
Ihor Kotliarevskyi <jagor0707@gmail.com>
2026-03-18 23:21:14 +0200

    update for deploy on Netlify

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 
-export default defineConfig({
-  base: "/cogen-calc-app/",
+export default defineConfig(({ mode }) => ({
+  base:
+    mode === "production" && import.meta.env.GITHUB_ACTIONS === "true"
+      ? "/cogen-calc-app/"
+      : "/",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-});
+}));
